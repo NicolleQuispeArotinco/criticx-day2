f0a54d7d81cb9736cc45268df9ba626c580c8c74
NicolleQuispeArotinco <nicolleqa3@gmail.com>
2020-05-20 11:29:45 -0500

    Add validations to the game table

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,2 +1,5 @@
 class Game < ApplicationRecord
+    validates :id, :name, :genre, :price, :release_date, presences: true
+    validates :genre, inclusion: { in: %w(Simulator Adventure Strategy Role-playing (RPG) Shooter Fighting Sport), message: "%Invalid genre"}
+    validates :price, numericality: { greater_than_or_equal_to: 0}
 end
