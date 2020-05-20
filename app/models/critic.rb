f1951ce220bbb144d3ccc05a251e8a4bf881b7be
NicolleQuispeArotinco <nicolleqa3@gmail.com>
2020-05-20 14:27:29 -0500

    correcting validations

--- app/models/critic.rb
+++ app/models/critic.rb
@@ -1,6 +1,7 @@
 class Critic < ApplicationRecord
   belongs_to :game
-  validates :created_at, :updated_at, :title, presence:true
+  validates :game_id, presence:true
   validates :username, length: { in: 2..12 }
-  validate :title, :body, presence: { message: 'Please write something!'}
+  validates :username , format: {with: /\w+/}
+  validates :title, :body, presence: { message: 'Please write something!'}
 end
--- app/models/game.rb
+++ app/models/game.rb
@@ -1,5 +1,5 @@
 class Game < ApplicationRecord
-    validates :id, :name, :genre, :price, :release_date, presence: true
-    validates :genre, inclusion: { in: %w(Simulator Adventure Strategy Role-playing (RPG) Shooter Fighting Sport), message: "%Invalid genre"}
-    validates :price, numericality: { greater_than_or_equal_to: 0}
+    validates :name, :genre, :price, :release_date, :company_id, presence: true
+    validates :genre, inclusion: { in: ["Simulator", "Adventure", "Strategy", "Role-playing (RPG)", "Shooter", "Fighting","Sport"]}
+    validates :price, numericality: { greater_than: 0}
 end
