12bbd58b9efba95e6d5af05581075bf788639c07
NicolleQuispeArotinco <nicolleqa3@gmail.com>
2020-05-20 11:48:45 -0500

    Add validations to the critic table

--- app/models/critic.rb
+++ app/models/critic.rb
@@ -1,3 +1,6 @@
 class Critic < ApplicationRecord
   belongs_to :game
+  validates :created_at, :updated_at, :title, presence:true
+  validates :username, length: { in: 2..12 }
+  validate :title, :body, presence: { message: 'Please write something!'}
 end
--- app/models/game.rb
+++ app/models/game.rb
@@ -1,5 +1,5 @@
 class Game < ApplicationRecord
-    validates :id, :name, :genre, :price, :release_date, presences: true
+    validates :id, :name, :genre, :price, :release_date, presence: true
     validates :genre, inclusion: { in: %w(Simulator Adventure Strategy Role-playing (RPG) Shooter Fighting Sport), message: "%Invalid genre"}
     validates :price, numericality: { greater_than_or_equal_to: 0}
 end
