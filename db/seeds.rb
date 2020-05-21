668d2db53bf535d033056d02950c5f647fa2c391
NicolleQuispeArotinco <nicolleqa3@gmail.com>
2020-05-20 19:36:06 -0500

    show a list of companies on JSON format

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Company.create([{id:1, name:"Code", country: "Peru"}, {id:2, name:"Codea", country: "Peru"}])
+Game.create([{id: 1, name: "Rune Factory 4 Special", genre: "Role-playing (RPG)", price: 5999, release_date: "2019-07-25", company_id: 1}, {id: 2, name: "Borderlands 3", genre: "Simulator", price: 5999, release_date: "2019-09-13", company_id: 2} ])
