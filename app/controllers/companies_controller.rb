5deaad111b9a2bbbba155ccf86f17945e29c9b6a
NicolleQuispeArotinco <nicolleqa3@gmail.com>
2020-05-20 21:19:51 -0500

    update some companies

--- app/controllers/companies_controller.rb
+++ app/controllers/companies_controller.rb
@@ -1,10 +1,19 @@
 class CompaniesController < ApplicationController
+    def update
+        @company = Company.find(params[:id])
+        if @company.update_attributes(companies_params)
+            render json: @company
+        else
+            render json: @company.errors, status: :unprocessable_entity
+        end
+    end
+=begin
     def destroy
         @company = Company.find(params[:id])
         @company.destroy
         render json: { status: 'Company destroyed', data: @company}, status: :ok
     end
-=begin
+    #-----
     def create
         @company = Company.new(companies_params)
         if @company.save
@@ -13,12 +22,13 @@ class CompaniesController < ApplicationController
             render json: @company.errors
         end
     end
-
+=end
     private
     def companies_params
         params.require(:company).permit(:name, :description, :start_date, :country)
     end
-
+    #----
+=begin
     def index
         @companies = Company.all
         render json: @companies
--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,11 +1,20 @@
 class GamesController < ApplicationController
+    def update
+        @game = Game.find(params[:id])
+        if @game.update_attributes(game_params)
+            render json: @game
+        else
+            render json: @game.errors, ststus: :unprocessable_entity
+        end
+    end
+    #------
+=begin
     def destroy 
         @game = Game.find(params[:id])
         @game.destroy
         render json: { status: 'Successfully destroyed', data: @game }, status: :ok
     end
     #------
-=begin
     def create 
         @game = Game.new(game_params)
         if @game.save
@@ -14,12 +23,13 @@ class GamesController < ApplicationController
             render json: @game.errors
         end
     end
-
+=end
     private
     def game_params
         params.require(:game).permit(:name, :genre, :price, :release_date, :company_id)
     end
     #------
+=begin
     def index
         @games = Game.all
         render json: @games
